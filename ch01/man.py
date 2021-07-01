8b0c28456eb62324ff053c83da510c28ebf3be3e
sssssssiiiiinnn <ygddjklollb@gmail.com>
2021-07-01 11:50:02 +0900

    add ch1

---
+++ ch01/man.py
@@ -0,0 +1,10 @@
+class Man:
+    def __init__(self, name):
+        self.name = "name"
+        print("Initialized")
+
+    def hello(self):
+        print(f"Hello {self.name}!!")
+
+    def goodbye(self):
+        print(f"Goodby {self.name}")
