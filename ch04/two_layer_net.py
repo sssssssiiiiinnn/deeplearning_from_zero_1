1ba2a4fe637c79ebf174e0bc4cac88684c21db71
sssssssiiiiinnn <ygddjklollb@gmail.com>
2021-07-04 19:06:49 +0900

    add train_neuralnet.py

--- ch04/two_layer_net.py
+++ ch04/two_layer_net.py
@@ -37,3 +37,36 @@ class TwoLayerNet:
         grads["W2"] = numerical_gradient(loss_W, self.params["W2"])
         grads["b2"] = numerical_gradient(loss_W, self.params["b2"])
         return grads
+
+    def gradient(self, x, t):  # コピっただけ
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+
+        batch_num = x.shape[0]
+
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+
+        dz1 = np.dot(dy, W2.T)
+        da1 = sigmoid_grad(a1) * dz1
+        grads['W1'] = np.dot(x.T, da1)
+        grads['b1'] = np.sum(da1, axis=0)
+
+        return grads
+
+    def accuracy(self, x, t):  # コピっただけ
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
